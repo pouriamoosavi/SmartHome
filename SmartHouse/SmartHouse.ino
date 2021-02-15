06db1c93d026419972241b78768547dea28d87fa
pouria.moosavi <pouria.moosvi@gmail.com>
2021-02-15 21:49:13 +0330

    Light

--- SmartHouse/SmartHouse.ino
+++ SmartHouse/SmartHouse.ino
@@ -2,11 +2,15 @@
 #include <Keypad.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <TimeLib.h>
+#include <DS3232RTC.h>
 
 const byte LCD_I2C_ADDR = 0x24;
 const byte KEYPAD_I2C_ADDR = 0x20;
 
 LiquidCrystal_I2C lcd(LCD_I2C_ADDR, 20, 4);
+
+DS3232RTC myRTC(false);
 
 char hexaKeys[4][3] = {
   {'1', '2', '3'},
@@ -23,6 +27,11 @@ Keypad_I2C mkp(makeKeymap(hexaKeys), rowPins, colPins, 4, 3, KEYPAD_I2C_ADDR);
 const uint8_t TEMP_MENU = 1;
 const uint8_t HUMI_MENU = 2;
 const uint8_t LIGHT_MENU = 3;
+const uint8_t SET_TIME_MENU = 3;
+
+const uint8_t LIGHT_MANUAL_MENU = 0;
+const uint8_t LIGHT_SENSOR_MENU = 1;
+const uint8_t LIGHT_TIME_MENU = 2;
 
 String inputMessage;
 
@@ -31,10 +40,16 @@ int desiredMenu = 0;
 int desiredTemp = 0;
 int desiredHumi = 0;
 int desiredLight = 0;
+int currentTime = 0;
+int currentHour = 0;
+int currentMinute = 0;
 
 int desiredTempTemp = 0;
 int desiredHumiTemp = 0;
 int desiredLightTemp = 0;
+int currentTimeTemp = 0;
+int currentHourTemp = 0;
+int currentMinuteTemp = 0;
 
 int* mkpPointTo;
 boolean mkpIsReady;
@@ -51,12 +66,15 @@ uint8_t heaterOn = 0;
 unsigned long waterStart = 0;
 unsigned long waterEnd = 0;
 uint8_t waterOn = 0;
+uint8_t lightOn = 0;
+uint8_t lightMenuStatus = 0;
 
 const uint8_t COOLER_PUMP_PIN = 5;
 const uint8_t COOLER_ENGINE_PIN = 4;
 const uint8_t COOLER_ENGINE_FAST_PIN = 3;
 const uint8_t HEATER_PIN = 2;
 const uint8_t WATER_PUMP_PIN = 6;
+const uint8_t LIGHT_BULB_PIN = 7;
 
 const uint8_t TEMP_SENSOR_PIN = 3;
 const uint8_t HUMI_SENSOR_PIN = 2;
@@ -70,6 +88,8 @@ void setup() {
   lcd.init();
   lcd.backlight();
 
+  setSyncProvider(RTC.get);
+
   pinMode(COOLER_PUMP_PIN, OUTPUT);
   pinMode(COOLER_ENGINE_PIN, OUTPUT);
   pinMode(COOLER_ENGINE_FAST_PIN, OUTPUT);
@@ -77,6 +97,7 @@ void setup() {
   pinMode(WATER_PUMP_PIN, OUTPUT);
   
   resetState();
+  Serial.println(hour());
 }
 
 void loop() {
@@ -98,6 +119,9 @@ void loop() {
       } else if (desiredMenu == LIGHT_MENU) {
         inputMessage = "Enter light: (" + String(desiredLight) + ")";
         mkpPointTo = &desiredLightTemp;
+        
+      } else if (desiredMenu == SET_TIME_MENU) {
+        inputMessage = "Set time: (" + String(currentTime) + ")";
       }
       
       mkpIsReady = false;
@@ -118,10 +142,33 @@ void loop() {
   checkTemp();
   checkHumi();
 
+  if(lightMenuStatus == LIGHT_SENSOR_MENU) {
+    checkLightSensor();
+  } else if (lightMenuStatus == LIGHT_TIME_MENU) {
+    checkLightTime();
+  }
+
   delay(10);
 }
 
 void getIntFromMkp(boolean echo) {
+  char key = mkp.getKey();
+  if(key) {
+    if(echo) {
+      lcd.print(key); 
+    }
+    if(key == '*') {
+      mkpIsReady = true;
+    } else if (key == '#') {
+      resetState();
+    } else {
+      *mkpPointTo = (*mkpPointTo * 10) + (key-'0');
+      mkpIsReady = false;
+    }
+  }
+}
+
+void getTimeFromMkp(boolean echo) {
   char key = mkp.getKey();
   if(key) {
     if(echo) {
@@ -157,6 +204,9 @@ void showMainMenu() {
   lcd.setCursor(0,2);
   String lcdLineThree = "3- Set light (" + String(desiredLight) + ")";
   lcd.print(lcdLineThree);
+  lcd.setCursor(0,3);
+  String lcdLineFour = "4- Set time (" + String(currentTime) + ")";
+  lcd.print(lcdLineFour);
 }
 
 void showOtherMenu() {
@@ -247,4 +297,29 @@ void checkHumi() {
   }
 
   digitalWrite(WATER_PUMP_PIN, waterOn);
+}
+
+void checkLightSensor() {
+  const int lightNow = analogRead(LIGHT_SENSOR_PIN);
+  if(!lightOn && lightNow < desiredLight) {
+    lightOn = 1;
+  }
+
+  if(lightOn && lightNow > desiredLight) {
+    lightOn = 0;
+  }
+
+  digitalWrite(LIGHT_BULB_PIN, lightOn);
+}
+
+void checkLightTime() {
+  if(true) {
+    lightOn = 1;
+  }
+
+  if(false) {
+    lightOn = 0;
+  }
+
+  digitalWrite(LIGHT_BULB_PIN, lightOn);
 }
